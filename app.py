85f9aba140a88f1b47ac620e487baa514663e833
Munjad <munjadbubere2006@gmail.com>
2025-03-15 00:55:56 +0530

    start

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Welcome</h1>"
+
+@app.route('/about')
+def about():
+    return "<h1>About us</h1>"
+
+if __name__ == '__main__':
+    
+    app.run(debug=True)
